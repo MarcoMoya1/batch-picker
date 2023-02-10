a4e210608d68c1600081890d724f31591eca3ccd
915marco <915marco@gmail.com>
2023-02-09 22:41:38 -0600

    date display added

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ var randomPicker = Math.floor(Math.random() * 3) + 1;
 function getRandomPicker() {
     alert(randomPicker);
 };
+
+new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"});
+
+function displayDate() {
+    document.getElementById("showDate").innerHTML = 
+    Date();
+}
